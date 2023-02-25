ec823a6d45fe629285a6cc453c6625e8f083f8e7
dndannynguyen <dn.dannynguyen@hotmail.com>
2023-02-24 17:15:47 -0800

    Implement calculator

---
+++ script.js
@@ -0,0 +1,44 @@
+function compute(id) {
+    console.log("Button" + id + "clicked!");
+    op1 = parseInt($("#op1").val());
+    op2 = parseInt($("#op2").val());
+    colorClass = ""
+
+    switch (id) {
+        case "addBtn":
+            $("#result").text(`Result of ${op1} + ${op2} = ${op1 + op2}`);
+            colorClass = "pink";
+            break;
+        case "subBtn":
+            $("#result").text(`Result of ${op1} - ${op2} = ${op1 - op2}`);
+            colorClass = "blue";
+            break;
+        case "mulBtn":
+            $("#result").text(`Result of ${op1} * ${op2} = ${op1 * op2}`);
+            colorClass = "green";
+            break;
+        case "divBtn":
+            $("#result").text(`Result of ${op1} / ${op2} = ${op1 / op2}`);
+            colorClass = "yellow";
+            break;
+        default:
+            break
+    }
+
+    $("#history").append(`<div class="${colorClass}">${$("#result").text()}<button class="rmvBtn"> Remove Me! </button> </div>`);
+    };
+
+
+
+const setup = () => {
+    console.log("Hello World");
+    $("button").click(function () {
+        compute(this.id)
+    });
+
+    $("body").on("click", ".rmvBtn", function () {
+        $(this).parent().remove()
+    });
+}
+
+$(document).ready(setup)
